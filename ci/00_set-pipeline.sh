16ac5166361b8d081a120cea3e77530f7a1a36bb
mwest44 <mwest@vmware.com>
2018-08-20 16:57:52 -0400

    after tom edits

--- ci/00_set-pipeline.sh
+++ ci/00_set-pipeline.sh
@@ -1,8 +1,8 @@
 #Used to Start the VMworld 2018 Concourse Pipeline
 #!/bin/bash
 
-fly -t main set-pipeline --pipeline acctctl --config pipeline.yml
+fly -t vsphereacct set-pipeline --pipeline acctctl --config pipeline.yml
 
 if [ $? -eq 0 ]; then
-	fly -t main unpause-pipeline --pipeline build-go-winery
+	fly -t vsphereacct unpause-pipeline --pipeline acctctl
 fi
