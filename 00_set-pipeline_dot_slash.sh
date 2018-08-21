d6c8d0ae86390984ed5b98497945e27735197720
Steve Tegeler <stegeler@vmware.com>
2018-08-20 22:26:32 -0700

    created new ./fly script

---
+++ 00_set-pipeline_dot_slash.sh
@@ -0,0 +1,8 @@
+#Used to Start the VMworld 2018 Concourse Pipeline
+#!/bin/bash
+
+fly -t vsphereacct set-pipeline --pipeline acctctl --config pipeline.yml
+
+if [ $? -eq 0 ]; then
+	fly -t vsphereacct unpause-pipeline --pipeline acctctl
+fi
