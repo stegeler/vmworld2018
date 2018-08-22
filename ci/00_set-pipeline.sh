faf9c1173944f1aad2706d8cba00011bcace1080
Tom Hite <thite@vmware.com>
2018-08-22 17:21:12 -0400

    fix params spec

--- ci/00_set-pipeline.sh
+++ ci/00_set-pipeline.sh
@@ -5,12 +5,13 @@
 # Note: change the -var values below to match your environment
 
 fly -t vsphereacct \
-  -var vcsa_user="root" \
-  -var vcsa_password='VMware1!' \
-  -var vcsa_address='vcsa-01a.corp.local' \
-  -var vcenter_admin_user='administrator@vsphere.local' \
   set-pipeline --pipeline acctctl \
-  --config pipeline.yml
+  --config pipeline.yml \
+  -var "vcsa_address=vcsa-01a.corp.local" \
+  -var "vcsa_user=root" \
+  -var "vcsa_password=VMware1!" \
+  -var "vcenter_admin_user=administrator@vsphere.local"
+  -var "vcenter_admin_password=VMware1!"
 
 if [ $? -eq 0 ]; then
 	fly -t vsphereacct unpause-pipeline --pipeline acctctl
