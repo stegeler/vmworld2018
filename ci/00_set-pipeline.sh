f9351926338c6e0dfda0c111110939fcbb6860cc
Tom Hite <thite@vmware.com>
2018-08-22 17:22:17 -0400

    derp: missing backslash

--- ci/00_set-pipeline.sh
+++ ci/00_set-pipeline.sh
@@ -10,7 +10,7 @@ fly -t vsphereacct \
   -var "vcsa_address=vcsa-01a.corp.local" \
   -var "vcsa_user=root" \
   -var "vcsa_password=VMware1!" \
-  -var "vcenter_admin_user=administrator@vsphere.local"
+  -var "vcenter_admin_user=administrator@vsphere.local" \
   -var "vcenter_admin_password=VMware1!"
 
 if [ $? -eq 0 ]; then
